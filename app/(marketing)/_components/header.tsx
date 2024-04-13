1a21561d8bcacc0de13c57feafb2e997018ca2f7
swajp <stejskalmira@icloud.com>
2024-04-14 00:08:16 +0200

    added link to logo

--- app/(marketing)/_components/header.tsx
+++ app/(marketing)/_components/header.tsx
@@ -9,17 +9,20 @@ import {
   UserButton,
 } from "@clerk/nextjs";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Header() {
   return (
     <header className="h-20 w-full border-b-2 border-slate-200 px-4">
       <div className="lg:max-w-screen-lg mx-auto flex items-center justify-between h-full">
-        <div className="pt-8 pl-4 pb-7 flex items-center gap-x-3">
-          <Image src="/penguino.svg" alt="Logo" width={40} height={40} />
-          <h1 className="text-2xl font-extrabold text-cyan-900 tracking-wide">
-            penguino
-          </h1>
-        </div>
+        <Link href="/">
+          <div className="pt-8 pl-4 pb-7 flex items-center gap-x-3">
+            <Image src="/penguino.svg" alt="Logo" width={40} height={40} />
+            <h1 className="text-2xl font-extrabold text-cyan-900 tracking-wide">
+              penguino
+            </h1>
+          </div>
+        </Link>
         <ClerkLoading>
           <Loader />
         </ClerkLoading>
