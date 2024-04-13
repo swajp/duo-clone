726793a6d464f4cd15226251c53faac6fdac2582
swajp <stejskalmira@icloud.com>
2024-04-14 01:24:48 +0200

    wrappers

--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -1,3 +1,22 @@
+import FeedWrapper from "@/components/feed-wrapper";
+import StickyWrapper from "@/components/sticky-wrapper";
+import Header from "./_components/header";
+import UserProgress from "@/components/user-progress";
+
 export default function LearnPage() {
-  return <div>LearnPage</div>;
+  return (
+    <div className="flex flex-row-reverse gap-[48px] px-6">
+      <StickyWrapper>
+        <UserProgress
+          activeCourse={{ title: "Spanish", imageSrc: "/flags/es.svg" }}
+          hearts={5}
+          points={100}
+          hasActiveSubscription
+        />
+      </StickyWrapper>
+      <FeedWrapper>
+        <Header title="Spanish" />
+      </FeedWrapper>
+    </div>
+  );
 }
