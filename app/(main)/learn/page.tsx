7acebd3e879580bbeb53ab8a63bbf237b9a4890a
swajp <stejskalmira@icloud.com>
2024-04-14 15:52:01 +0200

    displaying actual data

--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -17,14 +17,14 @@ export default async function LearnPage() {
     <div className="flex flex-row-reverse gap-[48px] px-6">
       <StickyWrapper>
         <UserProgress
-          activeCourse={{ title: "Spanish", imageSrc: "/flags/es.svg" }}
-          hearts={5}
-          points={100}
-          hasActiveSubscription
+          activeCourse={userProgress.activeCourse}
+          hearts={userProgress.hearts}
+          points={userProgress.points}
+          hasActiveSubscription={false}
         />
       </StickyWrapper>
       <FeedWrapper>
-        <Header title="Spanish" />
+        <Header title={userProgress.activeCourse.title} />
       </FeedWrapper>
     </div>
   );
