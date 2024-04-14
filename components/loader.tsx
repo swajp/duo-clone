f4e2a73c23149bcad870970ef58cfc5853043f8b
swajp <stejskalmira@icloud.com>
2024-04-14 13:20:50 +0200

    loader update

--- components/loader.tsx
+++ components/loader.tsx
@@ -1,5 +1,10 @@
+import { cn } from "@/lib/utils";
 import { LoaderIcon } from "lucide-react";
 
-export default function Loader() {
-  return <LoaderIcon className="h-5 w-5 text-muted-foreground animate-spin" />;
+export default function Loader({ className }: { className?: string }) {
+  return (
+    <LoaderIcon
+      className={cn("h-5 w-5 text-muted-foreground animate-spin", className)}
+    />
+  );
 }
